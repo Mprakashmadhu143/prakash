95522b3f90ad245f2a58395caed7585a9edbc67a
Mprakashmadhu143 <56971112+Mprakashmadhu143@users.noreply.github.com>
2019-10-24 21:26:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>STUDENTS ZONE - HOME</title>
+	<meta name= viewport content= "width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/s.css">
+	<link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet">
+	
+</head>
+<body>
+
+	<header>
+		<nav>
+			<div class="row clearfix">
+				<img src="images/l.png" class="logo">
+
+				<ul class="main-nav animated slideInDown" id="check-class">
+					<li><a href="#">HOME</a></li>
+					<li><a href="#">ABOUT</a></li>
+					
+				</ul>
+				
+			</div>
+		</nav>
+
+		<div class="main-content-header">
+			<h1> WELCOME TO <span class="colorchange">STUDENTS ZONE</span>.<br>
+				Well done is better than well said. </h1>
+			<a href="signup.php" class="btn btn-full"> SIGNUP</a>
+			<a href="log.php" class="btn btn-nav"> LOGIN</a>
+		</div>
+	</header>
+
+
+	</script>
+
+</body>
+</html>
